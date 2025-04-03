deb5e0c0d8fc4fbf20ace97d5ac5913f2195f40e
nalin.s <nalin.s@xenovex.com>
2025-04-03 11:58:32 +0530

    changes added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
-import { addTask, getTasks } from "./routes/taskRoutes";
+import taskRouter from "./routes/taskRoutes";
 import dotenv from "dotenv";
-import { auth, login, signup } from "./routes/authRoutes";
+import authRouter from "./routes/authRoutes";
 import logger from "./utils/logger";
 import path from "path";
 
@@ -11,10 +11,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.post("/signup", signup);
-app.post("/login", login);
-app.get("/tasks", auth, getTasks);
-app.post("/tasks", auth, addTask);
+app.use(authRouter);
+app.use(taskRouter);
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,52 +1,10 @@
-import { Request, Response } from "express";
+import express from "express";
+import { auth } from "../controllers/authControllers";
+import { addTask, getTasks } from "../controllers/taskControllers";
 
-import { connectTaskDB } from "../config/dbConfig";
-import { Task } from "../model/taskModel";
-import logger from "../utils/logger";
-import { base64ToLink, writePhotoFile } from "../utils/fileIOHelper";
+const taskRouter = express.Router();
 
-// Get all tasks
-export const getTasks = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const db = await connectTaskDB();
-    const tasks = await db.collection<Task[]>("tasks").find().toArray();
-    res.status(200).json(tasks);
-    logger.info("Get tasks da!", tasks);
-  } catch (err) {
-    res.status(500).send("DB error da!");
-    logger.error("Get tasks DB error da!", err);
-  }
-};
+taskRouter.get("/tasks", auth, getTasks);
+taskRouter.post("/tasks", auth, addTask);
 
-// Add a new task
-export const addTask = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { title, image } = req.body;
-    if (!title) {
-      res.status(400).send("Title venum da!");
-      logger.error("Title is missing da!");
-      return;
-    }
-
-    if (!image) {
-      res.status(400).send("Image venum da!");
-      logger.error("Image is missing da!");
-      return;
-    }
-
-    const db = await connectTaskDB();
-    const link = await base64ToLink(image, Date.now());
-    const newTask: Task = {
-      id: Date.now(),
-      title,
-      image: link,
-      done: false,
-    };
-    await db.collection("tasks").insertOne(newTask);
-    res.status(201).json(newTask);
-    logger.info("Task added da!", newTask);
-  } catch (err) {
-    res.status(500).send("DB error da!");
-    logger.error("Add task DB error da!", err);
-  }
-};
+export default taskRouter;
